704d6f64adb28ad70b509024ae0ae5c4883c99d3
Longer <draguitar@gmail.com>
2022-10-16 23:30:39 +0800

    main3 取得四個定位點

--- max_rectangle.py
+++ max_rectangle.py
@@ -12,15 +12,18 @@ for cnt in cnts:
     # 最大外接矩形
     x,y,w,h = cv2.boundingRect(cnt)
     cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
-    
+
     # rect = cv2.minAreaRect(cnt)
     # box = cv2.boxPoints(rect)
     # box = np.int0(box)
     # cv2.drawContours(img, [box],0, (0,0,255),2)
-    
+
 # 顯示圖片
 cv2.imshow('My Image', img)
 
 # 按下任意鍵則關閉所有視窗
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+[]
