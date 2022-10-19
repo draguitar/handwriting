a3474d662760802e6e23aec2a44b84b418ea2bbc
Longer <draguitar@gmail.com>
2022-10-19 23:54:50 +0800

    點名done

--- main2.py
+++ main2.py
@@ -1,7 +1,7 @@
 import cv2
 
 # img = cv2.imread('sample_0.png')
-img = cv2.imread('img/step_4_2.png')
+img = cv2.imread('Eng2.png')
 gray = cv2.cvtColor(img.copy(), cv2.COLOR_BGR2GRAY)
 _, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 blur = cv2.GaussianBlur(binary, (5, 5), 0)
@@ -18,34 +18,36 @@ print(f'矩形數量:{len(contours)}')
 draw_img = cv2.drawContours(img.copy(),contours,-1,(0,255,255),2)
 
 found = []
+center = []
 
 for i in range(len(contours)):
     rect = cv2.minAreaRect(contours[i])
     w = rect[1][0]
     h = rect[1][1]
-    
+
     if w and h:
         rate = min(w, h) / max(w, h)
         # 取定位矩形，非最外層
         # if (rate >= 0.75 and w < img.shape[1] / 4 and h < img.shape[0] / 4):
-        if rate >= 0.8 :  
+        if rate >= 0.8 :
             if hierarchy[0][i][2] != -1:
                 if hierarchy[0][i][2] >= 4:
                     found.append(i)
     else:
         pass
 print(found)
-print(hierarchy)
+# print(hierarchy)
 # --------------TODO--------------
 # 取最小外接舉行的中心點
 
 for i in found:
     rect = cv2.minAreaRect(contours[i])
-    center = tuple(int(iten) for iten in rect[0])
-    cv2.circle(draw_img, center, 2, (0, 0, 255), -1)
+    c = tuple(int(iten) for iten in rect[0])
+    center.append(c)
+    cv2.circle(draw_img, c, 2, (0, 0, 255), -1)
 
 
-
+print(center)
 # --------------TODO--------------
 
 
--- 點名/ocr.py
+++ 點名/ocr.py
@@ -1,30 +1,32 @@
-''''''  
+  
 import easyocr
 
 
 reader = easyocr.Reader(['en'], gpu=False)
-result = reader.readtext(r'./6.png', detail=0)
+ocr_result = reader.readtext(r'./6.png', detail=0)
 
 
 member_list = []
+attendance_list = []
 
-
-with open(r'./member_list.txt', 'w') as f:    
-    for m in result:
+with open(r'./attendance.txt', 'w') as f:    
+    for m in ocr_result:
         if len(m)>=4:
+            attendance_list.append(m)
             f.write(f'{m}\n')
-      
+
 with open(r'./人員名單.txt', 'r')  as f:
     data = f.read().splitlines()
- 
-final = [] 
+
+final_list = []
+  
+#final = [] 
+# m 出席者 - 長字串
+# d      名單 - 短字串
 for d in data:
-    for m in result:
-        if d not in m:
-            final.append(d)
-
-print(f'未出席清單：{final}')
-
+    for m in attendance_list:
+        if m.lower().find(d.lower())!=-1:
+            final_list.append(d)
+            break
+print(f'缺席者 {set(data) - set(final_list)}')
         
-
-
